bb067f5deab92fc1c9ac1452fb11482ce8287945
Firansa17 <132423194+Firansa17@users.noreply.github.com>
2024-08-07 14:36:04 +0700

    Add files via upload

---
+++ NetBeansProjects/modul2/src/main/java/com/mycompany/modul2/Modul2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.modul2;
+
+/**
+ *
+ * @author qeysa
+ */
+public class Modul2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void setVissible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
